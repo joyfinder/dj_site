a6abb1c68ada9403c0f57020619da9fb14783896
Ta-Liang Lu <ste176876@gmail.com>
2023-11-02 16:51:42 +0800

    Create catalog url

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def display_view(request):
+    return HttpResponse("hi. It is first page")
+
+    
